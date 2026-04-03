b785e31328cf846f7e3832bf66133522c5c68704
wassannc <balaram@wassan.org>
2026-04-03 21:57:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 FORMS = {
     "Farmer Register": {
-        "form_id": "farmer_register",
+        "form_id": "FarmerRegister_NF",
         "columns": ["name", "village"],
         "landscape_col": "plot_reg.landscape"
     },
     "Activities": {
-        "form_id": "activities",
+        "form_id": "Activities-NF",
         "columns": ["activity"],
         "landscape_col": "Primary_details.landscape"
     },
     "BRC": {
-        "form_id": "brc",
+        "form_id": "BRC_Units",
         "columns": ["center"],
         "landscape_col": "table_list_pd.landscape"
     }
