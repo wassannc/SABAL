e4258c7821625335dc3627f2c986710eacc0472b
wassannc <balaram@wassan.org>
2026-04-03 22:17:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,5 +13,30 @@ FORMS = {
         "form_id": "BRC_Units",
         "columns": ["center"],
         "landscape_col": "table_list_pd.landscape"
-    }
+    },
+    "Micro Enterprizes": {
+        "form_id": "Micro Enterprizes",
+        "columns": ["center"],
+        "landscape_col": "table_list_pd.landscape"
+    },
+    "Meetings&Trainings": {
+        "form_id": "Capacity_building",
+        "columns": ["center"],
+        "landscape_col": "CB_info.landscape"
+    },
+    "Intensification of Orchards": {
+        "form_id": "Orchards_Intensification",
+        "columns": ["center"],
+        "landscape_col": "basic_info.landscape"
+    },
+    "Agri Service Centers": {
+        "form_id": "Agri Service Centers",
+        "columns": ["center"],
+        "landscape_col": "pd-landscape"
+    },
+    "Large & Small Ruminants": {
+        "form_id": "Large_Small_Ruminants",
+        "columns": ["center"],
+        "landscape_col": "table_list_df.landscape"
+    } 
 }
