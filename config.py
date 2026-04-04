f23dc6c0456b1b3f7388be67b9daff73d4c0d1e9
wassannc <balaram@wassan.org>
2026-04-04 19:16:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ FORMS = {
     "Fingerlings Release": {
         "form_id": "1. Fingerlings Release",
         "columns": ["plot_reg.date","plot_reg.landscape","plot_reg.gp","plot_reg.village","plot_reg.farmer_name","plot_reg.spouse","plot_reg.season","plot_reg.crop_model","plot_reg.main_crop","plot_reg.sowing_date"],
-        "landscape_col": "plot_reg.landscape"
+        "district_col": "pd.district"
     },
     "Activities": {
         "form_id": "Activities-NF",
