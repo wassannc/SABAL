5b820075576b66e3880259570d01b94d2f59b28a
wassannc <balaram@wassan.org>
2026-04-04 16:44:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 from config import FORMS
 from utils import load_odk_data
-
-st.set_page_config(page_title="Project Dashboard", layout="wide")
+st.set_page_config(page_title="MIS Tracking-SABAL", layout="wide")
+st.title("📊 MIS Tracking - SABAL")
 
 st.sidebar.title("Menu")
 
