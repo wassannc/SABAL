df704057ac251b4c5d301ed505a852ca88a8bf69
wassannc <balaram@wassan.org>
2026-05-12 16:19:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 FORMS = {
-    "1. FarmerRegister-NF": {
+    "FarmerRegister_NF": {
         "form_id": "FarmerRegister_NF",
         "columns": ["plot_reg.date","plot_reg.landscape","plot_reg.gp","plot_reg.village","plot_reg.farmer_name","plot_reg.spouse","plot_reg.season","plot_reg.crop_model","plot_reg.main_crop","plot_reg.sowing_date"],
         "landscape_col": "plot_reg.landscape"
