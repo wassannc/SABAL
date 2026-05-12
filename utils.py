b861d747f202da058da2d40ffca0f06c0322759e
wassannc <balaram@wassan.org>
2026-05-12 12:16:21 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -4,14 +4,14 @@ import pandas as pd
 
 ODK_URL = st.secrets["ODK_URL"]
 USERNAME = st.secrets["USERNAME"]
-TOKEN = st.secrets["TOKEN"]
+PASSWORD = st.secrets["PASSWORD"]
 PROJECT_ID = st.secrets["PROJECT_ID"]
 
 @st.cache_data(ttl=300)
 def load_odk_data(form_id):
     url = f"{ODK_URL}/v1/projects/{PROJECT_ID}/forms/{form_id}.svc/Submissions"
     
-    TOKEN = st.secrets["TOKEN"]
+    response = requests.get(url, auth=(USERNAME, PASSWORD))
 
     if response.status_code != 200:
         st.error(f"Error: {response.status_code}")
