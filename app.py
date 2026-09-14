30235415b23165c0ad17f2123c5dc6fe401b6318
wassannc <balaram@wassan.org>
2026-09-15 03:25:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -144,7 +144,33 @@ def push_to_google_sheet(df):
             f"A{i+1}",
             data[i:i+chunk_size]
         ) 
+    
+@st.cache_data(ttl=600)
+def load_beneficiaries_data():
 
+    scope = [
+        "https://www.googleapis.com/auth/spreadsheets",
+        "https://www.googleapis.com/auth/drive"
+    ]
+
+    creds = Credentials.from_service_account_info(
+        st.secrets["gcp_service_account"],
+        scopes=scope
+    )
+
+    client = gspread.authorize(creds)
+
+    worksheet = client.open("Reminder_SABAL").worksheet("Beneficiaries")
+
+    all_data = worksheet.get_all_values()
+
+    if not all_data:
+        return pd.DataFrame()
+
+    headers = all_data[0]
+    rows = all_data[1:]
+
+    return pd.DataFrame(rows, columns=headers)
 st.sidebar.title("Menu")
 
 main_section = st.sidebar.radio(
