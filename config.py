76804c5371efd12b98e3208651f09209395303b5
wassannc <balaram@wassan.org>
2026-09-15 00:47:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,37 +11,37 @@ FORMS = {
         "column_labels": {"plot_reg.date": "Date","plot_reg.landscape": "Landscape","plot_reg.gp": "Panchayath","plot_reg.village": "Village","plot_reg.farmer_name": "Farmer name","plot_reg.spouse": "Spouse","plot_reg.season": "Season","plot_reg.crop_model": "Crop model","plot_reg.main_crop": "Main crop","plot_reg.sowing_date": "Sowing date","plot_reg-plot_ext": "Plot extent-acrs","plot_reg-plot_polygon": "GPS Location","plot_reg-crop_model": "Model crop name","plot_reg-main_crop": "Main crop","plot_reg-main_crop_others": "Main crop details","plot_reg-inter_crops": "Inter crops","plot_reg-intercrops_others": "Intercrops details","plot_reg-seed_treatment": "Seed treatement done?","plot_reg-border_crops_done": "Borader crops followed?"},
         "landscape_col": "plot_reg.landscape"
     },
-    "Activities": {
+    "1.1 Activities": {
         "form_id": "Activities-NF",
         "columns": ["Primary_details.date","Primary_details.landscape","Primary_details.gp","Primary_details.village","Primary_details.farmer_name","Primary_details.plot_ext","crop_activity","Nf_activites.nf_inputs","Nf_activites.Other_nf_input","Nf_activites.Qty_other_nfinput"],
         "landscape_col": "Primary_details.landscape"
     },
-    "BRC": {
+    "2. BRC": {
         "form_id": "BRC_Units",
         "columns": ["SubmissionDate","table_list_pd.landscape","table_list_pd.brc_unit","table_list_pd.product_name","table_list_pd.brc_sale_date","table_list_pd.dj_sale_farmer","table_list_pd.gender","table_list_pd.sale_village","table_list_sd.sale_qty","table_list_sd.total_income","table_list_cd.crops","table_list_cd.crop_ext"],
         "landscape_col": "table_list_pd.landscape"
     },
-    "Micro Enterprizes": {
+    "3. Micro Enterprizes": {
         "form_id": "Micro Enterprizes",
         "columns": ["SubmissionDate","table_list_pd.landscape","table_list_pd.gp","table_list_pd.village","table_list_pd1.farmer_name","table_list_pd1.processing_hub_tool","table_list_pd1.processing_date","table_list_pd1.processed_for","table_list_pd2.processing_farmer_village","table_list_pd2.processing_farmer","table_list_pd2.processing_qty_kgs","table_list_pd2.rent_amount","table_list_pd3.Data_sub_by"],
         "landscape_col": "table_list_pd.landscape"
     },
-    "Meetings&Trainings": {
+    "4. Meetings&Trainings": {
         "form_id": "Capacity_building",
         "columns": ["CB_info.landscape","CB_info.gp","CB_info.village","CB_info.Trainining_type","CB_info.Event_name","CB_info.Event_mode","Cb_info1.from_date","Cb_info1.days","Cb_info1.male","Cb_info1.female","Cb_info1.total_members","Cb_info1.Event_place"],
         "landscape_col": "CB_info.landscape"
     },
-    "Intensification of Orchards": {
+    "5. Intensification of Orchards": {
         "form_id": "Orchards_Intensification",
         "columns": ["SubmissionDate","basic_info.landscape","basic_info.gp","basic_info.village","basic_info.orchard_type","basic_info.farmer_add","type"],
         "landscape_col": "basic_info.landscape"
     },
-    "Agri Service Centers": {
+    "6. Agri Service Centers": {
         "form_id": "Agri Service Centers",
         "columns": ["SubmissionDate","pd.landscape","pd.gp","pd.village","farm_equipmnt_hired","ASC_Entp","ud.chc_equipmnt_rented_date","ud.chc_equipmnt_hired_farmer","ud.chc_equipmnt_total_hours_used","ud.total_hired_cost"],
         "landscape_col": "pd.landscape"
     },
-    "Large & Small Ruminants": {
+    "7. Large & Small Ruminants": {
         "form_id": "Large_Small_Ruminants",
         "columns": ["table_list_df.Month","table_list_df.Monthly_MIS","table_list_df.landscape","table_list_df.gp","table_list_df.village","table_list_df.livestock_type","table_list_df.Farmer"],
         "landscape_col": "table_list_df.landscape"
